ec09b5701d1af08ae040dc2b1b2708d3a6555f16
Paweł Walczak <waloproject24@gmail.com>
2023-09-24 19:18:28 +0200

    Commit-2

--- Person.java
+++ Person.java
@@ -2,5 +2,8 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("Hello1");
         System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello2");
+        System.out.println("Hello2");
     }
 }
